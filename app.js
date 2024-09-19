2d9df3f3e684acf254fc7075fa1eccc5b485d4e6
Haseeb ur Rehman <haseeburrehman763@gmail.com>
2024-09-19 05:57:31 +0500

    complete es6 features

--- app.js
+++ app.js
@@ -61,3 +61,77 @@
 // return total
 // };
 // console.log(sum(1, 2, 9, 5, 6, 7, 9)); // Output: 39
+
+
+
+// object short code ........................................ (destructuring)
+// normal way
+
+// const obj = {
+//     name : "haseeb",
+//     something: "hello",
+//     print : function(){
+//             console.log(this.something + this.name)
+//     }
+// }
+// const name = obj.name
+
+// object shortway 
+
+// const obj = {
+//     name : "haseeb",
+//     something: "hello",
+//     print : function(){
+//             console.log(this.something + this.name)
+//     }
+// }
+// // / Object Destructuring: Extracts 'name' property from 'obj' and assigns it to a variable 'name'
+// const { name } = obj;
+
+// console.log(name); // Logs 'Haseeb' to the console
+
+
+
+// array shortway
+
+// const arr = ["haseeb","rameez"]
+// const [firstName,lastName] = arr
+// console.log(firstName,lastName)
+
+// if you want 1 index or any other index
+// const arr = ["haseeb","rameez"]
+// const [,lastName] = arr
+// console.log(lastName)
+
+
+// spread operator ............................................................ (copy)
+// arr
+// const arr = ["haseeb","rameez","ali"]
+
+// const arrCopy = ["noor",...arr,"haider"]
+// console.log(arrCopy
+//     )
+// // obj
+// const obj = {
+//     name : "haseeb",
+//     something: "hello",
+// }
+
+// const objCopy = {...obj,hobby:"play cricket"}
+// console.log(objCopy)
+
+
+// classes (object bnaka deta)
+
+class Products {
+constructor(title,price){
+this.title = title 
+this.price= price
+}
+}
+
+const product1 = new Products("Led",12000)
+const product2 = new Products("Led 12",19000)
+
+const products = [product1,product2]
+products.forEach( p=> console.log(p.title) )
